bc8341d00221f858569f527e2df3ffca3333dc7c
David <david.diez31@free.fr>
2017-02-11 18:25:14 +0100

    ajout sidebar facebook twitter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # website 
 # N'oubliez pas de faire npm intall pour récupérer les dépendances (jquery et bootstrap).
+# et aussi npm install font-awesome --save.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-})
+    // David //
+    var offset = $("#sidebar").offset();
+    var topPadding = 200;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > offset.top) {
+            $("#sidebar").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $("#sidebar").stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+    //  //
+
+
+
+
+});
